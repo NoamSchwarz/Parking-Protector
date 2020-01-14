e967c2e31bd7431cae98681a965b7ced77a87c8b
Noam Schwarz <noam.n.schwarz@gmail.com>
2020-01-14 18:22:35 +0200

    created compare_images function

--- parkingMVP.py
+++ parkingMVP.py
@@ -2,6 +2,9 @@ import cv2
 import matplotlib.pyplot as plt
 import parking_proj_git.parking_class
 
+#TODO: run autocrop test with images 12 and 13 to see if change of thresh is needed for correct output
+#currently it gives small thing in parking instead of parking taken
+
 #TODO: have in parking_class as well. make global?
 imgScaleFactor = 0.7
 
@@ -9,7 +12,7 @@ def get_num_of_images():
     #TODO: Get the values automatically
     return 14
 
-def cropImage(imagePath, cropCoordinates):
+def crop_image(imagePath, cropCoordinates):
     cropTopRow, cropBottomRow, cropLeftColomn, cropRightColomn = cropCoordinates
     tempImage = cv2.imread(imagePath)
     tempImageResized = cv2.resize(tempImage, None, fx=imgScaleFactor, fy=imgScaleFactor, interpolation=cv2.INTER_LINEAR)
@@ -17,54 +20,53 @@ def cropImage(imagePath, cropCoordinates):
     tempImageCrop = tempImageResized[cropTopRow:cropBottomRow, cropLeftColomn:cropRightColomn]
     return tempImageCrop
 
+def compare_images(firstImage, secondImage):
+    # for notebook_sequential_images, witch are darker then the norebook from above
+    thresh = 65
+    maxVal = 255
+
+    difference = cv2.subtract(firstImage, secondImage)
+    differencGreyscale = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
+    retreval, differenceThreshold = cv2.threshold(differencGreyscale, thresh, maxVal, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(differenceThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    differenceThresholdCopy = differenceThreshold.copy()
+    differenceThresholdBGR = cv2.cvtColor(differenceThresholdCopy, cv2.COLOR_GRAY2BGR)
+    differenceWithContours = cv2.drawContours(differenceThresholdBGR, contours, -1, (0, 255, 0), 1);
+
+    return differenceWithContours, contours
+
 def parkingMVP():
     # test with multiple pictures and light change
     ESC_KEY = 27
     numOfImages = get_num_of_images()
 
-    #for notebook_sequential_images, witch are darker then the norebook from above
-    #TODO: run autocrop test with images 12 and 13 to see if change of thresh is needed for correct output
-    #currently it gives small thing in parking instead of parking taken
-    thresh = 65
-    maxVal = 255
-
-    #mark parking spot in picture with a rectangle
+    # mark parking spot in picture with a rectangle
+    # cropCoordinates is a tuple of the coordinates of the 4 corners of the rectangle
     markParking = parking_proj_git.parking_class.MarkParking()
     markParking.mark_parking()
-    #tuple of the coordinates of the 4 corners of the rectangle marking the parking spot
     cropCoordinates = markParking.get_rectangle_coordinates()
 
     # test with multiple pictures and light change
     #TODO: pass as parameter for parkingMVP function?
     firstImagePath = (r"C:\Users\noamn\Documents\shecodes\parking_project\parking_proj_git"
-                      r"\test pictures\notebook_sequential_images\image_{}.jpg".format(1))
+                      r"\test pictures\notebook_sequential_images\image_1.jpg")
+    firstImageCrop = crop_image(firstImagePath, cropCoordinates)
 
-    firstImageCrop = cropImage(firstImagePath, cropCoordinates)
-
-    for i in range(1,numOfImages+1):
+    for i in range(2, numOfImages+1):
 
         secondImagePath = (r"C:\Users\noamn\Documents\shecodes\parking_project\parking_proj_git" 
                           r"\test pictures\notebook_sequential_images\image_{}.jpg".format(i))
         # crop image_2
-        secondImageCrop = cropImage(secondImagePath, cropCoordinates )
+        secondImageCrop = crop_image(secondImagePath, cropCoordinates)
 
          # plt.figure(figsize=[15, 15])
         # plt.subplot(121); plt.imshow(firstImageCrop); plt.title("first image crop")
         # plt.subplot(122); plt.imshow(secondImageCrop); plt.title("second image crop")
         # plt.show()
 
-        # compare first_image to second_image , find contours
-        #TODO: put in sepeate function
-        #! A lot of these comments look like the block below them can be extracted into a separate,
-        #! named function. This would make the code easier to reason about and test.
-        difference = cv2.subtract(firstImageCrop, secondImageCrop)
-        differencGreyscale = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
-        retreval, differenceThreshold = cv2.threshold(differencGreyscale, thresh, maxVal, cv2.THRESH_BINARY)
-
-        contours, hierarchy = cv2.findContours(differenceThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        differenceThresholdCopy = differenceThreshold.copy()
-        differenceThresholdBGR = cv2.cvtColor(differenceThresholdCopy, cv2.COLOR_GRAY2BGR)
-        differenceWithContours = cv2.drawContours(differenceThresholdBGR, contours, -1, (0, 255, 0), 1);
+        # compare firstImageCrop to secondImageCrop, find contours and mark them on image
+        differenceWithContours, contours = compare_images(firstImageCrop, secondImageCrop)
 
         #incase compared photos are exactly/mostly the same and no contours are found
         #TODO: can this block be a seperate function?
@@ -82,7 +84,7 @@ def parkingMVP():
         else:
             print("small thing in image {}".format(i))
 
-        #TODO: can be seperate function
+        #TODO: can be seperate function? or part of try-except block? (this block needs biggestContour)
         #draw boundindgBox abour biggest contour
         topLeftX, topLeftY, width, hight = cv2.boundingRect(biggestContour)
         topLeftCoror = (topLeftX,topLeftY)
