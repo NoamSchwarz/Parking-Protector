f72f21b57d32e76b085beb8e77df003b911e8599
Noam Schwarz <noam.n.schwarz@gmail.com>
2020-01-29 10:40:11 +0200

    added ALL_SPOTS_EMPTY_IMG and deleted a bunch of old files

--- parkingMVP.py
+++ parkingMVP.py
@@ -8,14 +8,11 @@ import os.path
 #TODO change parking_class to ParkingMark
 #TODO find a funner name for parkingMVP - parking protector?
 
+#TODO if I change the file to notebook_sequential_images then get_num_of_images cant fint the path for some reason
 IMG_FILE_PATH = os.path.abspath("notebook_sequential_images_V2")
-
 IMAGE_PATH_TEMPLATE = IMG_FILE_PATH + "\image_{}.jpg"
-
-FIRST_IMAGE_PATH = IMAGE_PATH_TEMPLATE.format(1)
-
+ALL_SPOTS_EMPTRY_IMG = IMG_FILE_PATH + r"\all_spots_empty.jpg"
 IMG_RESIZE_FACTOR = 0.7
-
 
 #TODO: understand how this works because i copied it from stackoverflow
 def get_num_of_images(img_file_path):
@@ -31,8 +28,8 @@ def crop_image(img, parking_mark_object):
     temp_image_crop = img[crop_top_row:crop_bottom_row, crop_left_colomn:crop_right_colomn]
     return temp_image_crop
 
-#TODO put IMG_RESIZE_FACTOR into read_image() , shouldn't be a paramete
-def read_image(img_path, img_resize_factor):
+def read_image(img_path):
+    img_resize_factor = IMG_RESIZE_FACTOR
     temp_image = cv2.imread(img_path)
     temp_image_resize = cv2.resize(temp_image, None, fx=img_resize_factor, fy=img_resize_factor, interpolation=cv2.INTER_LINEAR)
     return temp_image_resize
@@ -52,25 +49,24 @@ def compare_images(first_image, second_image, image_index):
     difference_threshold_bgr = cv2.cvtColor(difference_threshold_copy, cv2.COLOR_GRAY2BGR)
     difference_with_contours = cv2.drawContours(difference_threshold_bgr, contours, -1, (0, 255, 0), 1);
 
-    biggest_contour = find_biggest_contour_area(contours, image_index)
+    biggest_contour = find_biggest_contour(contours, image_index)
     draw_contour_bounding_box(biggest_contour, difference_with_contours)
 
     cv2.imshow("image {}".format(image_index), difference_with_contours)
     cv2.waitKey(1500)
     cv2.destroyWindow("image {}".format(image_index))
 
-    #return difference_with_contours, contours
     return biggest_contour
 
-# mark parking spot in picture with a rectangle
-def find_parking_spot():
-    base_image = read_image(FIRST_IMAGE_PATH, IMG_RESIZE_FACTOR)
+
+def mark_parking_spot():
+    base_image = read_image(ALL_SPOTS_EMPTRY_IMG)
     parking_mark = ParkingMark(base_image)
     parking_mark.mark_parking()
     return parking_mark
 
-#TODO change to find_biggest_contour because the area calculation is done in is-parking_taken
-def find_biggest_contour_area(contour_list, image_index):
+
+def find_biggest_contour(contour_list, image_index):
     try:
         biggest_contour = max(contour_list, key=cv2.contourArea)
     except ValueError:
@@ -80,7 +76,6 @@ def find_biggest_contour_area(contour_list, image_index):
 
 
 def is_parking_taken(biggest_contour, parking_area_img,image_index):
-    #TODO check again: what exactly is the unit that contourArea returns
     if biggest_contour is False:
         return
     #area of contours in units of non-zero pixels
@@ -104,21 +99,19 @@ def draw_contour_bounding_box(biggest_contour, difference_with_contours_img):
 
 def main():
     num_of_images = get_num_of_images(IMG_FILE_PATH)
-    #TODO : SEPERATE FIRST IMAGE FROM REST OF THE IMAGES.  call it empty parking lot, so we know it has a special perpous
-    # ( empty parking lot can simply be a copy of the first image in the file)
-    parking_mark = find_parking_spot()
+    parking_mark = mark_parking_spot()
 
-    first_image = read_image(FIRST_IMAGE_PATH, IMG_RESIZE_FACTOR)
+    first_image = read_image(IMAGE_PATH_TEMPLATE.format(1))
     first_image_crop = crop_image(first_image, parking_mark)
 
-    #TODO: replace get_num_of _images with function of read_next_image which will not be dependant on the source of the
+    #TODO: replace get_num_of _images with function of read_next_image which will not be dependant on the source of the images
     #TODO read_next_image will replace the current read_image function
     #images, just on the location of the directory the images are in.
     #TODO: replace for loop with while wihch is dependant on get_next_image
     #(for example, if there are no more images, get_next_image will return False, and the loop will exit)
-    for image_index in range(2, num_of_images+1):
+    for image_index in range(2, num_of_images):
 
-        second_image = read_image(IMAGE_PATH_TEMPLATE.format(image_index), IMG_RESIZE_FACTOR)
+        second_image = read_image(IMAGE_PATH_TEMPLATE.format(image_index))
         second_image_crop = crop_image(second_image, parking_mark)
 
         biggest_contour = compare_images(first_image_crop, second_image_crop, image_index)
